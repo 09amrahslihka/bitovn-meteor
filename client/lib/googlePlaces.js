931ae63537c03b4a7ef19f39efbf8841a372fb21
MeteorBirds <developer@vayuz.com>
2018-02-06 11:05:42 +0530

    Google Geo Locaation Update

--- client/lib/googlePlaces.js
+++ client/lib/googlePlaces.js
@@ -1,5 +1,5 @@
 GoogleMaps.init({
-  'sensor': false, //optional
+  // 'sensor': false, //optional
   'key': 'AIzaSyBnQrXrRyWtQu7EKB9Hk1S10Gc6eQuamUo', //optional
   'language': 'en',  //optional
   'libraries': 'places'
